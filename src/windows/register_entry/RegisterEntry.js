3b06b2db5912a243732a0afcec967fcbfe7df30b
Kamran <mkamran4786@gmail.com>
2024-10-08 09:23:38 +0500

    - design frontend for uploading images

--- src/windows/register_entry/RegisterEntry.js
+++ src/windows/register_entry/RegisterEntry.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, {useState, useEffect, useRef} from "react";
 import { useForm } from "react-hook-form";
 import { makeStyles } from "@material-ui/core/styles";
 import Autocomplete from "@material-ui/lab/Autocomplete";
@@ -10,6 +10,7 @@ import { keybind_form } from "../../hooks/keybind.js";
 import { validate_int, validate_required } from "../Custom/validate.js";
 import Notification from "../Custom/Notification.js";
 import { base } from "../../proxy_url.js";
+import './Register_entry.css'
 
 const useStyles = makeStyles({
   root: {
@@ -32,11 +33,12 @@ export const getDate = () => {
 };
 
 export default function RegisterEntry() {
+  const uploadInputRef = useRef();
 
   const { type, supplier: supplierFromParams } = useParams();
-  
+
   // Determine `selectSupplier` based on `type`
-  const selectSupplier = type === "select"; 
+  const selectSupplier = type === "select";
   const supplierParsed = selectSupplier ? null : JSON.parse(supplierFromParams);
 
   const { register, handleSubmit, reset } = useForm();
@@ -52,6 +54,17 @@ export default function RegisterEntry() {
   const [notificationOpen, setNotificationOpen] = useState(false);
   const [initialRender, setInitialRender] = useState(true);
   const date = getDate();
+  const [imageSrc, setImageSrc] = useState(null);
+
+  // Function to handle file input change
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file && file.type.startsWith('image/')) {
+      const imageURL = URL.createObjectURL(file);
+      setImageSrc(imageURL);
+      // TODO: after uploading the image you can send it to backend and fetch the data to load into the form
+    }
+  };
 
   useEffect(() => {
     setKeyBinds();
@@ -110,107 +123,131 @@ export default function RegisterEntry() {
   return (
     <>
       <Home />
-      <div className="entry_content">
-        <div className="form-box">
-          {selectSupplier ? (
-            <h3 style={{ display: "inline" }}>Register Entry</h3>
-          ) : (
-            <h3 style={{ display: "inline" }}>Supplier Name: {selectedSupplier?.name}</h3>
-          )}
-        </div>
-        <form onSubmit={handleSubmit(OnSubmit)}>
-          <div className="form-box">
-            <div>
-              <TextInput
-                label="Bill Number"
-                type="number"
-                id="bill_number"
-                errorText={error.bill_number.message}
-                props={{ inputProps: { ...register("bill_number") } }}
-                errorState={error.bill_number.error}
-              />
-            </div>
-            <div>
-              <TextInput
-                label="Bill Date"
-                type="date"
-                defaultValue={date}
-                errorState={false}
-                errorText="Invalid Date"
-                props={{ inputProps: { ...register("register_date") } }}
-              />
-            </div>
-            {selectSupplier && 
-              (
-                <Autocomplete
-                  options={suppliers}
-                  getOptionLabel={(option) => option.name}
-                  onChange={(event, value) => setSelectedSupplier(value)}
-                  autoHighlight
-                  onKeyPress={(e) => {
-                    if (e.key === "Enter") {
-                      document.getElementById("parties").focus();
-                      e.preventDefault();
-                      // write your functionality here
-                    }
-                  }}
-                  renderInput={(params) => (
-                    <TextInput
-                      label="Select Supplier"
-                      props={params}
-                      errorState={error.supplier?.error}
-                      errorText={error.supplier?.message}
-                    />
-                  )}
-                />
-              )}
-            <div>
-              <Autocomplete
-                options={parties}
-                id="parties"
-                getOptionLabel={(parties) => parties.name}
-                onChange={(event, value) => setParty(value)}
-                autoHighlight
-                onKeyPress={(e) => {
-                  if (e.key === "Enter") {
-                    document.getElementById("amount").focus();
-                    e.preventDefault();
-                    // write your functionality here
-                  }
-                }}
-                renderInput={(params) => (
-                  <TextInput
-                    label="Party Name"
-                    props={params}
-                    custom={register("party")}
-                    errorState={error.party.error}
-                    errorText={error.party.message}
-                  />
-                )}
-              />
-            </div>
-            <div>
-              <TextInput
-                label="Amount"
-                id="amount"
-                type="number"
-                errorState={error.amount.error}
-                errorText={error.amount.message}
-                props={{ inputProps: { ...register("amount") } }}
-              />
-            </div>
-            <input type="submit" className="button" />
+      <div className="register-entry-wrapper">
+        <div className="form-wrapper">
+          <div className="box-title" style={{marginRight: '120px'}}>
+            {selectSupplier ? (
+                <h3 style={{display: "inline"}}>Register Entry</h3>
+            ) : (
+                <h3 style={{display: "inline"}}>Supplier Name: {selectedSupplier?.name}</h3>
+            )}
           </div>
-        </form>
+          <div className="form-box">
+            <form onSubmit={handleSubmit(OnSubmit)}>
+              <div className="form-box">
+                <div>
+                  <TextInput
+                      label="Bill Number"
+                      type="number"
+                      id="bill_number"
+                      errorText={error.bill_number.message}
+                      props={{inputProps: {...register("bill_number")}}}
+                      errorState={error.bill_number.error}
+                  />
+                </div>
+                <div>
+                  <TextInput
+                      label="Bill Date"
+                      type="date"
+                      defaultValue={date}
+                      errorState={false}
+                      errorText="Invalid Date"
+                      props={{inputProps: {...register("register_date")}}}
+                  />
+                </div>
+                {selectSupplier &&
+                    (
+                        <Autocomplete
+                            options={suppliers}
+                            getOptionLabel={(option) => option.name}
+                            onChange={(event, value) => setSelectedSupplier(value)}
+                            autoHighlight
+                            onKeyPress={(e) => {
+                              if (e.key === "Enter") {
+                                document.getElementById("parties").focus();
+                                e.preventDefault();
+                                // write your functionality here
+                              }
+                            }}
+                            renderInput={(params) => (
+                                <TextInput
+                                    label="Select Supplier"
+                                    props={params}
+                                    errorState={error.supplier?.error}
+                                    errorText={error.supplier?.message}
+                                />
+                            )}
+                        />
+                    )}
+                <div>
+                  <Autocomplete
+                      options={parties}
+                      id="parties"
+                      getOptionLabel={(parties) => parties.name}
+                      onChange={(event, value) => setParty(value)}
+                      autoHighlight
+                      onKeyPress={(e) => {
+                        if (e.key === "Enter") {
+                          document.getElementById("amount").focus();
+                          e.preventDefault();
+                          // write your functionality here
+                        }
+                      }}
+                      renderInput={(params) => (
+                          <TextInput
+                              label="Party Name"
+                              props={params}
+                              custom={register("party")}
+                              errorState={error.party.error}
+                              errorText={error.party.message}
+                          />
+                      )}
+                  />
+                </div>
+                <div>
+                  <TextInput
+                      label="Amount"
+                      id="amount"
+                      type="number"
+                      errorState={error.amount.error}
+                      errorText={error.amount.message}
+                      props={{inputProps: {...register("amount")}}}
+                  />
+                </div>
+                <input type="submit" className="button"/>
+              </div>
+            </form>
+          </div>
+        </div>
+        <div className={`upload-box ${imageSrc ? '' : 'align-center'}`}>
+          {!imageSrc && <button className="general-btn" onClick={() => uploadInputRef.current.click()}>
+            Upload Photos
+          </button>}
+          <input className="input-hide" ref={uploadInputRef} type='file' onChange={handleFileChange} />
+          {imageSrc && <div className="img-uploaded">
+            <div className="box-title" style={{margin: '0 auto'}}>
+              <h3 style={{display: "inline"}}>Bill Photo</h3>
+            </div>
+            <div style={{margin: '15px 0'}}>
+              <img src={imageSrc} width="400" height="550"/>
+            </div>
+            <div className="next-photo-btn">
+            <button className="general-btn" onClick={() => {
+              }}>
+                Next Photos
+              </button>
+            </div>
+          </div>}
+        </div>
       </div>
       <Notification
-        status={status}
-        setError={setError}
-        notificationOpen={notificationOpen}
-        setNotificationOpen={setNotificationOpen}
-        initialRender={initialRender}
-        setInitialRender={setInitialRender}
-        reset={resetRegisterEntry}
+          status={status}
+          setError={setError}
+          notificationOpen={notificationOpen}
+          setNotificationOpen={setNotificationOpen}
+          initialRender={initialRender}
+          setInitialRender={setInitialRender}
+          reset={resetRegisterEntry}
       />
     </>
   );
